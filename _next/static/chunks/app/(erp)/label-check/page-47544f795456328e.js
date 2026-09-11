dd911bb2bef83505bd8ab909d3c20adc6e799599
TatGold Deployment <deployment@localhost>
2026-09-11 15:32:18 +0530

    Add section navigation and paged items to entry forms

---
+++ _next/static/chunks/app/(erp)/label-check/page-47544f795456328e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[638],{65144:function(n,e,u){Promise.resolve().then(u.bind(u,58074))}},function(n){n.O(0,[848,9762,9642,1837,4800,383,2971,2117,1744],function(){return n(n.s=65144)}),_N_E=n.O()}]);
